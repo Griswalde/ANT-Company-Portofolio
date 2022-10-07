ba856cf4e6e4389ac47745e85eda36eedaa1fc6d
Griswalde <agunggriswald158@gmail.com>
2022-10-08 06:58:39 +0700

    membuat page testimonial

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import react from "react";
 import Navbar from "./component/Navbar/Navbar";
 import Home from "./component/Home/Home";
 import About from "./component/About/About";
+import Service from "./component/Service/Service";
+import Testimonial from "./component/Testimonial/Testimonial";
 const App = ()=>{
   return( 
     <>
     <Navbar/>
     <Home/>
     <About/>
+    <Service/>
+    <Testimonial/>
     </>
   );
 }
---
+++ src/component/Testimonial/Testimonial.js
@@ -0,0 +1,168 @@
+import React,{useState} from 'react'
+import "./testimonial.css"
+
+
+
+
+const Testimonial = () => {
+
+    const data = [
+        {
+            "img":"https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+            "content":"it is not every dayy that you come across a passionate and trustworthly financial advisor",
+            "name": "Luke",
+            "position":"designer"
+        },
+        {
+            "img":"https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+            "content":"it is not every dayy that you come across a passionate and trustworthly financial advisor",
+            "name":"Luke",
+            "position":"designer"
+        },
+        {
+            "img":"https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+            "content":"it is not every dayy that you come across a passionate and trustworthly financial advisor",
+            "name":"Luke",
+            "position":"designer"
+        },
+        {
+            "img":"https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+            "content":"it is not every dayy that you come across a passionate and trustworthly financial advisor",
+            "name":"Luke",
+            "position":"designer"
+        },
+        {
+            "img":"https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+            "content":"it is not every dayy that you come across a passionate and trustworthly financial advisor",
+            "name":"Luke",
+            "position":"designer"
+        },
+        {
+            "img":"https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+            "content":"it is not every dayy that you come across a passionate and trustworthly financial advisor",
+            "name":"Luke",
+            "position":"designer"
+        },
+        {
+            "img":"https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+            "content":"it is not every dayy that you come across a passionate and trustworthly financial advisor",
+            "name":"Luke",
+            "position":"designer"
+        },
+        {
+            "img":"https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+            "content":"it is not every dayy that you come across a passionate and trustworthly financial advisor",
+            "name": "Luke",
+            "position":"designer"
+        },
+        {
+            "img":"https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+            "content":"it is not every dayy that you come across a passionate and trustworthly financial advisor",
+            "name": "Luke",
+            "position":"designer"
+        },
+        {
+            "img":"https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+            "content":"it is not every dayy that you come across a passionate and trustworthly financial advisor",
+            "name": "Luke",
+            "position":"designer"
+        },
+        {
+            "img":"https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+            "content":"it is not every dayy that you come across a passionate and trustworthly financial advisor",
+            "name": "Luke",
+            "position":"designer"
+        },
+        {
+            "img":"https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+            "content":"it is not every dayy that you come across a passionate and trustworthly financial advisor",
+            "name": "Luke",
+            "position":"designer"
+        },
+        {
+            "img":"https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+            "content":"it is not every dayy that you come across a passionate and trustworthly financial advisor",
+            "name": "Luke",
+            "position":"designer"
+        },
+        {
+            "img":"https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+            "content":"it is not every dayy that you come across a passionate and trustworthly financial advisor",
+            "name": "Luke",
+            "position":"designer"
+        },
+        {
+            "img":"https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+
+            "content":"it is not every dayy that you come across a passionate and trustworthly financial advisor",
+            "name": "Luke",
+            "position":"designer"
+        },
+    ];
+
+    const [showMorePost,setShowMorePost] = useState(6)
+
+    const loadMore = ()=> {
+        setShowMorePost((prev) => prev + 3);
+    };
+
+
+
+  return (
+    <div className='container testimonial-section'>
+        <div className='section_title'>
+                <h5>Testimonial</h5>
+                <span className="line"></span>
+                </div>
+
+                <div className='row'>
+                    {data.slice(0,showMorePost).map((item,index)=>(
+                        <div className='col-xl-4 col-lg-4 col-md-6 col-sm-12'>
+
+                            <div className='content-card'>
+                                <img src={item.img} alt="image"/>
+                                <p>
+                                    {item.content} </p>
+                                     <p>
+
+                                     <span className='name'>{item.name}</span>
+                                     </p>
+                                    
+                                    <p>{item.position}</p>
+                                
+
+                            </div>
+
+
+                            </div>
+                    ))}
+
+                    {showMorePost>=data.length ? null : (
+                        <span className='laod-mores-button' onClick={loadMore}>
+                        Load More
+                </span>
+                    )}
+
+                    
+
+
+                </div>
+    </div> 
+  );
+};
+
+export default Testimonial
