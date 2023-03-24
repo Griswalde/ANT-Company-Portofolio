1ae50ee46ea12b8f245be734749d7813d175cef9
Griswalde~ <agunggriswald158@gmail.com>
2023-03-25 06:00:34 +0700

    perbaikan yang lain

--- src/component/AntMedia/Media.js
+++ src/component/AntMedia/Media.js
@@ -1,31 +1,15 @@
 import React from "react";
 import "./media.css";
 import Typewriter from "typewriter-effect";
-import LightSpeed from 'react-reveal/LightSpeed';
+import LightSpeed from "react-reveal/LightSpeed";
 
-
-
-
-
-
-
-const Home = ({theme,handleChangeTheme}) => {
+const Home = ({ theme, handleChangeTheme }) => {
   return (
     <div className="container-fluid soon">
-
-  
-
-    <LightSpeed left cascade>
-
-    
-
-      <div className="container soon-content">
-        <h1>COMING SOON</h1>
-      
-
-      
-        
-      </div>
+      <LightSpeed left cascade>
+        <div className="container soon-content">
+          <h1>COMING SOON</h1>
+        </div>
       </LightSpeed>
     </div>
   );
